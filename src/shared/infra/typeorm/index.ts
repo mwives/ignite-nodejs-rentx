c3a748ca255b600b4aa658d002419c7888e87ccd
Ives M. Watanabe <ivesmw@gmail.com>
2022-01-05 16:12:47 -0300

    change: database config for production docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -4,12 +4,11 @@ import {
   getConnectionOptions,
 } from "typeorm";
 
-async function createConnection(host = "db"): Promise<Connection> {
+async function createConnection(): Promise<Connection> {
   const defaultOptions = await getConnectionOptions();
 
   return createTypeORMConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
