1e939fc6a0b21c7160fba834edc9037d8dd1e080
Ives M. Watanabe <ivesmw@gmail.com>
2022-01-05 18:09:28 -0300

    fix: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,6 +2,7 @@ import "express-async-errors";
 import "reflect-metadata";
 import "@shared/container";
 
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
@@ -17,6 +18,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
